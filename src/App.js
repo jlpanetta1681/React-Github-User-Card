6ad85d0504ce40bb2e82cd88a1e661607166de97
Joe-Panetta <jlpanetta1681@icloud.com>
2020-09-29 16:22:37 -0400

    Working on fetching data,and storing in state from github/users

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+class App extends React.Component {
+  state = {
+    users: []
+  }
+      
+      fetch("https://api.github.com/users/jlpanetta1681")
+    };
+
+  }
+
+  
+  
+  
+  
+  
+  
+  
+  render() {}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
