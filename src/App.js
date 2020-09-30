8d14e505777e4d5a97c693b7042067da93a0ac38
Joe-Panetta <jlpanetta1681@icloud.com>
2020-09-30 00:40:47 -0400

    MVP completed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
 import React from 'react';
 import './App.css';
-
+import axios from 'axios';
+import FollowerList from './components/FollowerList'
 
 class App extends React.Component {
-  state = {
-    users: []
-  }
-      
-      fetch("https://api.github.com/users/jlpanetta1681")
+  constructor() {
+    super();
+    this.state = {
+      user: {login: '', avatar_url: ''},
+      userText: "",
+      followers: []
     };
-
   }
 
-  
-  
-  
-  
-  
-  
-  
-  render() {}
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  componentDidMount() {
+    axios.get('https://api.github.com/users/jlpanetta1681')
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        user: res.data
+      });
+    })
+    .catch(err => console.log(err));
+  }
+  handleChanges = e => {
+    const { value } = e.target;
+    this.setState({
+      userText: value
+    });
+  };
+
+  getFollowers = e => {
+    e.preventDefault()
+    axios.get('https://api.github.com/users/jlpanetta1681/followers')
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        followers: res.data
+      });
+    })
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Github Users</h1>
+        </header>
+        <div className="Button-Container">
+        <button onClick={this.getFollowers}>Get Followers</button>
+        </div>
+
+        <div className="users">
+            <img src={this.state.user.avatar_url} alt="user"/>
+            <p>{this.state.user.login}</p> 
+         </div>
+         <FollowerList followers={this.state.followers}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App; 
